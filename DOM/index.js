38bc09fca3eb08be3b3ab08966e443f255ebfcdc
HaoDuong1994 <hao.dv5370@gmail.com>
2024-07-08 16:25:59 +0700

    commit

--- DOM/index.js
+++ DOM/index.js
@@ -1,88 +1,107 @@
 const data = [
-    {
-        id: 0,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 3000,
-        sold: 5
-    },
-    {
-        id: 1,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 2,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 3,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 4,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 5,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 6,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 5
-    },
-    {
-        id: 7,
-        image: 'https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn',
-        title: 'E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)',
-        price: 35000,
-        sold: 0
-    }
+  {
+    id: 0,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 3000,
+    sold: 5,
+  },
+  {
+    id: 1,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 2,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 3,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 4,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 5,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 6,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 5,
+  },
+  {
+    id: 7,
+    image:
+      "https://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tnhttps://cf.shopee.vn/file/vn-11134207-23030-2fbdeow3mxovf7_tn",
+    title:
+      "E - Sở hữu điện thoại chính hãng Xiaomi 12 Pro 5G chọn màu ngẫu nhiên ( đọc kỹ mô tả)",
+    price: 35000,
+    sold: 0,
+  },
 ];
 
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
 function createCard(cardInfo) {
-    const { image, title, price, sold } = cardInfo;
+  const { image, title, price, sold } = cardInfo;
 
-    return `
+  return `
         <div class="card">
             <img src="${image}" class="card-image" alt="">
             <span class="card-title">${title}</span>
             <div class="card-bottom">
                 <span class="card-bottom-left">đ${price}</span>
-                <span class="card-bottom-right">${sold > 0 ? "Đã bán " + sold : ""}</span>
+                <span class="card-bottom-right">${
+                  sold > 0 ? "Đã bán " + sold : ""
+                }</span>
             </div>
         </div>
     `;
 }
 
 function renderData(data) {
-    let str = '';
+  let str = "";
 
-    data.forEach(element => {
-        const newCardString = createCard(element);
-        str += newCardString;
-    });
+  data.forEach((element) => {
+    const newCardString = createCard(element);
+    str += newCardString;
+  });
 
-    container.innerHTML = str;
+  container.innerHTML = str;
 }
+let a = 0;
 
 renderData(data);
